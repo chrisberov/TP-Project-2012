2a87abf9d1bde59a7674fef42087ba4ecf71d57f
Chris Berov <chrisberov@gmail.com>
2012-03-11 20:44:53 +0200

    added new menu

--- Tank Wars/src/Game/MainClass.java
+++ Tank Wars/src/Game/MainClass.java
@@ -1,30 +1,36 @@
 package Game;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainClass {
+	static Image img;
 	
 	public static void main(String[] args) {
 		GameMenu();
+		
 		//GameWindow();
 		
 	}
 	
 	public static void GameMenu() {
-		ImagePanel panel = new ImagePanel(new ImageIcon("D:/menu.jpg").getImage());
-	    JFrame frame = new JFrame();
+		ImagePanel panel = new ImagePanel(new ImageIcon("D:/bg.png").getImage());
+	    JFrame frame = new JFrame();    
 	    frame.setSize(1030, 615);
 	    frame.getContentPane().add(panel);
 	    frame.setResizable(false);
 	    frame.setVisible(true);
+	    
 	}
 	
 	public static void GameWindow() {
@@ -34,12 +40,14 @@ public class MainClass {
 		frame.setSize(1030,615);
 		frame.setVisible(true);
 		frame.setResizable(false);
+		
 	}
+	
 }
 	
 	class ImagePanel extends JPanel {
 
-		  private Image img;
+		  private Image img, imgplay, imgoptions, imgcredits, imgexit;
 
 		  public ImagePanel(String img) {
 		    this(new ImageIcon(img).getImage());
@@ -56,7 +64,16 @@ public class MainClass {
 		  }
 		  
 		  public void paintComponent(Graphics g) {
+			  	imgplay    = new ImageIcon("D:/play.png").getImage();
+			  	imgoptions = new ImageIcon("D:/options.png").getImage();
+			  	imgcredits = new ImageIcon("D:/credits.png").getImage();
+			  	imgexit    = new ImageIcon("D:/exit.png").getImage();
+			  	
 			    g.drawImage(img, 0, 0, null);
-			  }
+			    g.drawImage(imgplay,    440, 280, null);
+			    g.drawImage(imgoptions, 410, 360, null);
+			    g.drawImage(imgcredits, 410, 440, null);
+			    g.drawImage(imgexit,    450, 520, null);
+		  }
 	}
 
